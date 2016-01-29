15d34f950b2b1516129b1a055c91c4791e8d7564
Vasyl Vavrychuk <vvavrychuk@gmail.com>
2016-01-29 11:55:02 +0200

    do something usefull

--- src/main/java/kom/App.java
+++ src/main/java/kom/App.java
@@ -1,13 +1,17 @@
 package kom;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.integration.TrapezoidIntegrator;
+
+public class App {
+  public static void main(String[] args) {
+    UnivariateFunction f = new UnivariateFunction() {
+      public double value(double x) {
+        return Math.sin(x);
+      }
+    };
+    TrapezoidIntegrator integrator = new TrapezoidIntegrator();
+    double value = integrator.integrate(10000, f, 0, Math.PI);
+    System.out.println(value);
+  }
 }
